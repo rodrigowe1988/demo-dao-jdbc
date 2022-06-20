19bacc9b81aa341b8c51678e7b7b3d4fb7e998fe
rodrigowe1988 <rodrigoweber97@hotmail.com>
2022-06-20 16:00:19 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -14,6 +16,8 @@ public class Program {
 
         Seller seller = new Seller(21, "Bob", "bob@email.com", new Date(), 3000.0, obj);
         System.out.println(seller);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
     }
 }
